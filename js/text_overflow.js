7b09b29682342586ccf997a87607e2a4b8fad112
汪世傑 <louise87276@gmail.com>
2024-07-28 12:36:18 +0800

    test

--- js/text_overflow.js
+++ js/text_overflow.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const items = document.querySelectorAll(".change_item");
   items.forEach((item) => {
     if (item.textContent.length > 90) {
-      //把前面的空格刪掉 不然再分割 index是錯的
+      //把text前面的空格刪掉 不然slice的時候index是錯的
       item.textContent = item.textContent.trimStart();
       item.textContent = item.textContent.slice(0, 90) + "...";
     }
